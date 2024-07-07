3d4024fabaa815e25b577bcc05cf30ecb0ab7280
FarazAhmad2 <farazahmad574@gmail.com>
2024-07-07 12:21:40 +0530

    first commit

---
+++ src/components/WeatherDisplay.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const WeatherDisplay = ({ weather }) => {
+  const { temperature, conditions } = weather;
+
+  return (
+    <div>
+      <p style={{ color: temperature > 20 ? "red" : "blue" }}>
+        Temperature: {temperature}°C
+      </p>
+      <p>Conditions: {conditions}</p>
+    </div>
+  );
+};
+
+export default WeatherDisplay;
